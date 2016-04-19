f72638d5acf3c6beb5bec828f6dac6573c5e6cfd
Jared Vu <j_vu4@u.pacific.edu>
2016-04-19 16:50:04 -0700

    barebones
    - Uniform Cross
    - Weighted Random Choice
    - Needs work on Mutation
    - Working Fitness Function

--- stockMarket_GenAlg.py
+++ stockMarket_GenAlg.py
@@ -7,11 +7,12 @@
 from yahoo_finance import Share
 import numpy
 import random
+import operator
 
 # Set the Constants ---------------
 StartingDate = '2015-04-01'
 EndingDate = '2016-04-14'
-PopulationSize = 10
+PopulationSize = 200
 DataSize = 0
 NumberOfGenerations = 0
 MutationRate = 5
@@ -19,13 +20,33 @@ MutationChange = 2
 #----------------------------------
 
 class Chromosome():
-   def __init__(self, min=None, max=None, prev_min=None, prev_max=None, buy=None, score =None):
+    def __init__(self, min=None, max=None, prev_min=None, prev_max=None, buy=None, score =None):
         self.min = min
         self.max = max
         self.prev_min = prev_min
         self.prev_max = prev_max
         self.buy = buy
         self.score = score
+
+    def mutate(self):
+        mu, sigma = 0, 0.15 # mean and standard deviation
+        s = numpy.random.normal(mu, sigma, 1)
+        x = iter(s)
+        toChange = random.randint(0,5)
+        if toChange == 0:
+           self.buy = random.randint(0,999)%2
+        if toChange == 1:
+            self.min = x.next()
+        if toChange == 2:
+            self.max = x.next()
+        if toChange == 3:
+            self.prev_min = x.next()
+        if toChange == 4:
+            self.prev_max = x.next()
+        if self.min > self.max:
+            self.min, self.max = self.max, self.min
+        if self.prev_min > self.prev_max:
+            self.prev_min, self.prev_max = self.prev_max, self.prev_min
 
 class TrainingData(object):
     population = []
@@ -57,7 +78,7 @@ class TrainingData(object):
             file.write(str((float(cArray[x])-float(oArray[x+1]))/100) + ' ' + str((float(cArray[x+1]) - float(oArray[x+2]))/100) + ' ' + str((float(oArray[x]) - float(oArray[x+1]))) + '\n')
 
             self.dayChange.append((float(cArray[x])-float(oArray[x+1]))/100)
-            self.nextDayChange.append(float(cArray[x+1]) - float(oArray[x+2])/100)
+            self.nextDayChange.append((float(cArray[x+1]) - float(oArray[x+2]))/100)
             self.profit.append(float(oArray[x]) - float(oArray[x+1]))
         #Makes sure the population size is
         DataSize = len(self.dayChange)
@@ -90,6 +111,8 @@ class TrainingData(object):
         for i in range(PopulationSize):
             match = False
             for j in range(DataSize):
+
+                #print(self.population[i].min, self.nextDayChange[j], self.population[i].max)
                 #If match is found we BUY
                 if(self.population[i].prev_min < self.dayChange[j] and self.population[i].prev_max > self.dayChange[j]):
                     if(self.population[i].min < self.nextDayChange[j] and self.population[i].max > self.nextDayChange[j]):
@@ -107,23 +130,90 @@ class TrainingData(object):
                 #We have not found any matches = -5000
                 if match == False:
                     self.population[i].score = -5000
+            #print(self.population[i].score)
 
     def weighted_random_choice(self):
         self.fitnessFunction()
-        max = sum(self.population.score for self.population in self.population)
+        max = self.population[0].score
+        for i in self.population[1:]:
+            max+= i.score
         pick = random.uniform(0,max)
         current = 0
         for i in range(len(self.population)):
             current += self.population[i].score
             if current > pick:
-                return self.population[i]
+                self.nextGeneration.append(self.population[i])
 
+    def exists(self):
+        for i in range(len(self.population)):
+            if self.population[i].score is None:
+                self.population.pop(i)
+                i -= 1
 
-class GeneticAlgorithm(object):
-    pass
+    def uniformCross(self, z):
+        children = []
+        for i in range(PopulationSize-len(self.nextGeneration)):
+            child = Chromosome(0,0,0,0,0)
+            chromosome1 = self.nextGeneration[random.randint(0,999999) % len(self.nextGeneration)]
+            chromosome2 = self.nextGeneration[random.randint(0,999999) % len(self.nextGeneration)]
+            if(random.randint(0,999) %2):
+                child.min = chromosome1.min
+            else:
+                child.min = chromosome2.min
+
+            if(random.randint(0,999) %2):
+                child.max = chromosome1.max
+
+            else:
+                child.max = chromosome2.max
+
+            #Check to make sure the swap yields viable chromosome
+            if child.max < child.min:
+                    child.max, child.min = child.min, child.max
+
+            if(random.randint(0,999) %2):
+                child.prev_min = chromosome1.prev_min
+            else:
+                child.prev_min = chromosome2.prev_min
+
+            if(random.randint(0,999) %2):
+                child.prev_max = chromosome1.prev_max
+            else:
+                child.prev_max = chromosome2.prev_max
+
+            #Check if swap is needed
+            if child.prev_max < child.prev_min:
+                    child.prev_max, child.prev_min = child.prev_min, child.prev_max
+
+            if(random.randint(0,999) %2):
+                child.buy = chromosome1.buy
+            else:
+                child.buy = chromosome2.buy
+
+            #Append
+            children.append(child)
+
+        # #Mutation
+        for i in range(len(children)):
+            if random.randint(0,999) % 100 <= z:
+                children[i].mutate()
+        self.population[i] = children[i]
+        for i in range(len(children),len(self.population),1):
+            self.population[i] = self.nextGeneration[i-len(children)]
+        self.fitnessFunction()
+        self.population.sort(key=operator.attrgetter('score'))
+
+        self.exists()
+
+    def printChromosomes(self):
+        for i in range(len(self.population)):
+            print(self.population[i].score)
 
 if __name__ == '__main__':
     x = TrainingData()
     x.generateData()
     x.populationInit()
+    x.weighted_random_choice()
+    x.uniformCross(MutationRate)
+    x.printChromosomes()
 
